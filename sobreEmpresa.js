ca823a730122b39532b8bee4d5d64e349608071e
caueribasapolinario <caueapolinariogold@gmail.com>
2024-10-23 19:53:18 -0300

    SobreEmpresa pronto

--- sobreEmpresa.js
+++ sobreEmpresa.js
@@ -1,4 +1,4 @@
-const URL = "https://utmu0l1z.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%22sobreEmpresa%22%5D";
+const URL = "https://utmu0l1z.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%22sobreEmpresa%22%5D%0A";
 
 window.addEventListener("load", async () => {
     const wrapper = document.querySelector("div.txt-box-text")
@@ -9,22 +9,31 @@ window.addEventListener("load", async () => {
 
     const data = await result.json();
 
+    console.log(data.result)
+
     data.result.forEach(element => {
-        const div = MontarSobre(element);
+        montarSobre(element);
     });
 });
 
-function MontarSobre(element) {
+function montarSobre(element) {
+    console.log(element)
+
     const h2 =document.createElement("h2");
     h2.innerText = element.Descricao;
 
+    console.log(h2)
+
     const h3 = document.createElement("h3");
     h3.innerText = element.FraseFinal;
+    
+    const br1 = document.createElement('br')
+    const br2 = document.createElement('br')
 
-    const div = document.createElement("div");
-    div.classList.add("valor");
-    div.append(h2, h3);
 
-    return div;
+    const div = document.getElementsByClassName('txt-box-text')[0]
+   
+    div.append(h2, br1, br2,  h3);
 
+    console.log(div)
 }
